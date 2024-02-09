c00431aaa571c106b015693f4dea6f6f0f8ff5be
Juan Marcos <juanmarcosleon1990@gmail.com>
2024-02-09 19:41:41 +0000

    Rutas y composables creados y agregados al navigationGraph.

---
+++ app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/activities/LogIn.kt
@@ -0,0 +1,9 @@
+package com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun logIn(navController: NavHostController) {}
+
+
---
+++ app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/navigation/NavigationGraph.kt
@@ -0,0 +1,64 @@
+package com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.credits
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.game
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.invitations
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.jukeBox
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.logIn
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.match
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.profile
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.score
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.searchBar
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.selectMatch
+import com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.activities.signUp
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun navigationGraph() {
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routs.LogIn.rout) {
+
+        composable(Routs.LogIn.rout) {
+            logIn(navController = navController)
+        }
+        composable(Routs.SignUp.rout) {
+            signUp(navController = navController)
+        }
+        composable(Routs.Profile.rout) {
+            profile(navController = navController)
+        }
+        composable(Routs.Game.rout) {
+            game(navController = navController)
+        }
+        composable(Routs.SearchBar.rout) {
+            searchBar(navController = navController)
+        }
+        composable(Routs.Match.rout) {
+            match(navController = navController)
+        }
+        composable(Routs.SelectMatch.rout) {
+            selectMatch(navController = navController)
+        }
+        composable(Routs.Score.rout) {
+            score(navController = navController)
+        }
+        composable(Routs.Invitations.rout) {
+            invitations(navController = navController)
+        }
+        composable(Routs.JukeBox.rout) {
+            jukeBox(navController = navController)
+        }
+        composable(Routs.Credits.rout) {
+            credits(navController = navController)
+        }
+    }
+}
