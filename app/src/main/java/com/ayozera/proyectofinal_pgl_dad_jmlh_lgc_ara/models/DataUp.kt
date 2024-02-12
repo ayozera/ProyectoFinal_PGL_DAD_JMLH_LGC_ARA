aa79ce8d255e2077d519823a98cba9e15209a665
ayozera <rodalvayoformativo@gmail.com>
2024-02-12 13:44:01 +0000

    diseñando player activity

--- app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/models/DataUp.kt
+++ app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/models/DataUp.kt
@@ -3,15 +3,14 @@ package com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.models
 class DataUp {
     companion object {
         fun getSongs(): ArrayList<Song> {
-            val lista = arrayListOf(
+            return arrayListOf(
                 Song("Chop Suey", "chop-suey"),
                 Song("Rise up", "rise_up"),
                 Song("Respira el momento", "respira_el_momento"),
-                Song("Adentro", "Multiviral"),
+                Song("Adentro", "adentro"),
                 Song("Jeremias17.5", "jeremias17_5"),
                 Song("Maquiavelico", "maquiavelico")
             )
-            return lista
         }
     }
 }
