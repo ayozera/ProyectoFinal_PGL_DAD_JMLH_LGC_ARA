b80b0ff512de135594edbea144b8d41e9d2d5da1
Shurnoah <Lorenagarcia.lg13@gmail.com>
2024-02-12 15:34:27 +0000

    games description interface first version

--- app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/models/DataUp.kt
+++ app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/models/DataUp.kt
@@ -1,5 +1,7 @@
 package com.ayozera.proyectofinal_pgl_dad_jmlh_lgc_ara.models
 
+import android.content.Context
+
 class DataUp {
     companion object {
         fun getSongs(): ArrayList<Song> {
@@ -10,6 +12,16 @@ class DataUp {
                 Song("Adentro", "Multiviral"),
                 Song("Jeremias17.5", "jeremias17_5"),
                 Song("Maquiavelico", "maquiavelico")
+            )
+            return lista
+        }
+        fun getComments(current: Context): ArrayList<Comment> {
+            val lista = arrayListOf(
+                Comment("Ana", "Me encanta este juego", "2021-12-12"),
+                Comment("Luis", "Es muy entretenido", "2021-12-12"),
+                Comment("Juan", "No me gusta", "2021-12-12"),
+                Comment("Maria", "Es muy dificil", "2021-12-12"),
+                Comment("Pedro", "Es muy facil", "2021-12-12")
             )
             return lista
         }
