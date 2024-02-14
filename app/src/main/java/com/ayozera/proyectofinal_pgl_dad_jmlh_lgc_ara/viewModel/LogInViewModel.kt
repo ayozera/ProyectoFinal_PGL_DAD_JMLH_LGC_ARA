a6967498fd70196adc8f0f3af7d20a5e019bf347
Juan Marcos <juanmarcosleon1990@gmail.com>
2024-02-14 22:39:54 +0000

    Añadida pantalla de bienvenida

--- app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/viewModel/LogInViewModel.kt
+++ app/src/main/java/com/ayozera/proyectofinal_pgl_dad_jmlh_lgc_ara/viewModel/LogInViewModel.kt
@@ -12,9 +12,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class LogInViewModel(private val appMainViewModel: AppMainViewModel) : ViewModel() {
+class LogInViewModel() : ViewModel() {
 
-    private val auth: FirebaseAuth = Firebase.auth
+//    private val auth: FirebaseAuth = Firebase.auth
     private val _loading = MutableStateFlow(false)
 
 /*private var _user = MutableStateFlow("")
@@ -23,28 +23,28 @@ class LogInViewModel(private val appMainViewModel: AppMainViewModel) : ViewModel
 private var _password = MutableStateFlow("")
     val password = _password.asStateFlow()*/
 
-    fun signInWithEmailAndPassword(email: String, password: String, home: () -> Unit) =
-        viewModelScope.launch {
-            try {
-                auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d("LogInViewModel", "Cuenta Creada con Exito")
-                            home()
-                        } else {
-                            _loading.value = false
-                            Log.d(
-                                "LogInViewModel",
-                                "Ha ocurrido un error: ${task.result.toString()}"
-                            )
-                        }
-                    }
-
-        }catch (ex: Exception){
-            Log.d("LogInViewModel", "Ha ocurrido un error: ${ex.message}")
-        }
-
-        }
+//    fun signInWithEmailAndPassword(email: String, password: String, home: () -> Unit) =
+//        viewModelScope.launch {
+//            try {
+//                auth.signInWithEmailAndPassword(email, password)
+//                    .addOnCompleteListener { task ->
+//                        if (task.isSuccessful) {
+//                            Log.d("LogInViewModel", "Cuenta Creada con Exito")
+//                            home()
+//                        } else {
+//                            _loading.value = false
+//                            Log.d(
+//                                "LogInViewModel",
+//                                "Ha ocurrido un error: ${task.result.toString()}"
+//                            )
+//                        }
+//                    }
+//
+//        }catch (ex: Exception){
+//            Log.d("LogInViewModel", "Ha ocurrido un error: ${ex.message}")
+//        }
+//
+//        }
 
 
 
